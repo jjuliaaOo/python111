4b6d9b767c53978c0c6e17fcb27c13ee970eb6f0
jjuliaaOo <144954092+jjuliaaOo@users.noreply.github.com>
2023-09-15 12:16:21 +0300

    Update дз1.3.py

--- дз1.3.py
+++ дз1.3.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(-6, 6.25, 0.25)
+
+x = np.arange(-6, 6.1, 0.1)
 plt.figure(figsize=(5, 5))
 plt.xlabel(r'$x$')
 plt.ylabel(r'$f(x)$')
-plt.title('график')
+plt.title('график функции')
+plt.xlim([-7, 7])
+
 plt.plot(x, 1.1**(-2*x)*np.sin(x))
 plt.grid(True)
 plt.show()
