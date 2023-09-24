fabb80c94210041e1224c6316c06d4787610645b
jjuliaaOo <144954092+jjuliaaOo@users.noreply.github.com>
2023-09-24 19:27:36 +0300

    Create дз3.СФ1.py

---
+++ дз3.СФ1.py
@@ -0,0 +1,23 @@
+import math
+f = open('C:/Users/admin/Downloads/СФ1.txt', 'r')
+max = 0
+t=[]
+
+for line in f:
+  t.append(int(line))
+n = t[0] + 1
+for i in range(1, n):
+  if t[i] >= max:
+    max = t[i]
+f.close()
+
+a = abs(max)
+j = 0
+while a > 0:
+  if a % 10 == 0:
+    j = j + 1
+  a = a // 10
+
+f1 = open('result.txt', 'w')
+f1.write(str(j))
+f1.close()
